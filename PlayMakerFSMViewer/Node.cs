8e9d7b7618955400fede4da2e74fab216cc588c9
5FiftySix6 <ybham6@gmail.com>
2018-08-10 00:27:54 -0400

    Colors (state/transition) + Boxes for transitions

--- PlayMakerFSMViewer/Node.cs
+++ PlayMakerFSMViewer/Node.cs
@@ -3,10 +3,12 @@ using PlayMakerFSMViewer.FieldClasses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -22,23 +24,48 @@ namespace PlayMakerFSMViewer
         public FsmTransition[] transitions;
         public string name;
         private bool selected;
+        
+        private readonly Color[] _stateColors = 
+        {
+            // Color.FromScRgb(1, 0.5f, 0.5f, 0.5f), 
+            // Color.FromScRgb(1, 0.545098066f, 0.670588255f, 0.9411765f),
+            // Color.FromScRgb(1, 0.243137255f, 0.7607843f, 0.6901961f),
+            // Color.FromScRgb(1, 0.431372553f, 0.7607843f, 0.243137255f),
+            // Color.FromScRgb(1, 1f, 0.8745098f, 0.1882353f),
+            // Color.FromScRgb(1, 1f, 0.5529412f, 0.1882353f),
+            // Color.FromScRgb(1, 0.7607843f, 0.243137255f, 0.2509804f),
+            // Color.FromScRgb(1, 0.545098066f, 0.243137255f, 0.7607843f)
+            Color.FromRgb(128, 128, 128), // grey
+            Color.FromRgb(116, 143, 201), // blue
+            Color.FromRgb(58, 182, 166), // cyan
+            Color.FromRgb(93, 164, 53), // green
+            Color.FromRgb(225, 254, 50), // yellow
+            Color.FromRgb(235, 131, 46), // orange
+            Color.FromRgb(187, 75, 75), // red
+            Color.FromRgb(117, 53, 164) // purple
+        };
+        
+        private readonly Color[] _transitionColors = 
+        {
+            Color.FromRgb(222, 222, 222), // grey
+            Color.FromRgb(197, 213, 248), // blue
+            Color.FromRgb(159, 225, 216), // cyan
+            Color.FromRgb(183, 225, 159), // green
+            Color.FromRgb(225, 254, 102), // yellow
+            Color.FromRgb(255, 198, 152), // orange
+            Color.FromRgb(225, 159, 160), // red
+            Color.FromRgb(197, 159, 225) // purple
+        };
+
         public bool Selected
         {
-            get
-            {
-                return selected;
-            }
+            get => selected;
             set
             {
                 selected = value;
-                if (selected)
-                {
-                    rectPath.StrokeThickness = 2;
-                }
-                else
-                {
-                    rectPath.StrokeThickness = 1;
-                }
+                rectPath.StrokeThickness = selected 
+                    ? 2 
+                    : 1;
             }
         }
 
@@ -70,11 +97,7 @@ namespace PlayMakerFSMViewer
             this.transitions = transitions;
 
             this.name = name;
-            foreach (FsmTransition transition in transitions)
-            {
-                name += "\n" + transition.fsmEvent.name;
-            }
-
+            
             grid = new Grid();
             grid.SetValue(Canvas.LeftProperty, transform.X);
             grid.SetValue(Canvas.TopProperty, transform.Y);
@@ -86,7 +109,7 @@ namespace PlayMakerFSMViewer
                 RadiusY = 1
             };
 
-            rectPath = new Path()
+            rectPath = new Path
             {
                 Fill = fill,
                 Stroke = stroke,
@@ -95,18 +118,59 @@ namespace PlayMakerFSMViewer
                 Data = rectGeom
             };
 
-            label = new Label()
+            FontFamily font = new FontFamily("Segoe UI Bold");
+
+            StackPanel stack = new StackPanel();
+
+            byte cIndex = (byte) state.Get("colorIndex").GetValue().AsUInt();
+
+            label = new Label
             {
-                Foreground = Brushes.Gray,
+                Foreground = Brushes.White,
                 Content = name,
                 Padding = new Thickness(1),
-                FontFamily = new FontFamily("Segoe UI Bold"),
+                FontFamily = font,
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1, 1, 1, 0),
                 HorizontalAlignment = HorizontalAlignment.Stretch,
-                HorizontalContentAlignment = HorizontalAlignment.Center
+                HorizontalContentAlignment = HorizontalAlignment.Center,
+                VerticalContentAlignment = VerticalAlignment.Stretch,
+                Background = new SolidColorBrush(_stateColors[cIndex]),
+                MaxWidth = transform.Width,
+                MinWidth = transform.Width
             };
 
+            stack.Children.Add(label);
+            
+            foreach (FsmTransition transition in transitions)
+            {
+                stack.Children.Add(new Label
+                {
+                    Background = new SolidColorBrush(_transitionColors[cIndex]),
+                    Foreground = Brushes.DimGray,
+                    Content = transition.fsmEvent.name,
+                    Padding = new Thickness(1),
+                    BorderBrush = Brushes.Black,
+                    BorderThickness = new Thickness(1, .5, 1, .25),
+                    FontFamily = font,
+                    HorizontalAlignment = HorizontalAlignment.Stretch,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Stretch,
+                    MaxWidth = transform.Width,
+                    MinWidth = transform.Width
+                });
+            }
+
+            List<Label> list = stack.Children.OfType<Label>().ToList();
+            for (int index = 0; index < list.Count; index++)
+            {
+                Label i = list[index];
+                Grid.SetRow(i, index);
+                i.MaxHeight = i.MinHeight = transform.Height / list.Count;
+            }
+
             grid.Children.Add(rectPath);
-            grid.Children.Add(label);
+            grid.Children.Add(stack);
             //graphCanvas.Children.Add(grid);
         }
     }
