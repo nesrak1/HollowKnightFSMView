bab19763f098cc7cbdb51acda700402f673d96b3
5FiftySix6 <ybham6@gmail.com>
2018-08-11 07:12:10 -0400

    Fix for empty fsm strings

--- PlayMakerFSMViewer/ActionReader.cs
+++ PlayMakerFSMViewer/ActionReader.cs
@@ -38,7 +38,7 @@ namespace PlayMakerFSMViewer
             ParamDataType type, int paramDataPos, int paramByteDataSize, BinaryReader reader)
         {
             string displayValue = "? " + type;
-            if (version == 1) //read binary as normal
+            if (version == 1 && !(type == ParamDataType.FsmString && paramByteDataSize == 0)) //read binary as normal
             {
                 switch (type)
                 {
